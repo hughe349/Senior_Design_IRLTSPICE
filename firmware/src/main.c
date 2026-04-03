977663f11b2e8b045c54972a44aa42b4384b7910
MicahSamuel77 <m.samuel0922@gmail.com>
2026-04-03 00:00:16 -0400

    feat: prefix/message testing

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -9,9 +9,11 @@
 #include <stdbool.h>
 
 // temp funcs
-void setup_blinker(void);
+// void setup_blinker(void);
 void send_array(uint8_t *data, uint32_t size);
 void send_string(char *str);
+static inline void to_instruction(instruction_t *instruction, uint8_t value);
+
 
 // defines
 // #define START_STOP_CHAR 64
@@ -44,54 +46,61 @@ int main(void)
   setup_gpios();
 
   // var inits for loop
-  uint8_t arrays[9][128];
-  int current_array = -1;
+  // uint8_t arrays[9][128];
+  // int current_array = -1;
+  uint8_t rx;
+  instruction_t instruction;
 
-  // in while loop vvvvv
   while (true) {  
     if ((USART5->ISR & USART_ISR_RXNE)) {
       uint8_t rx = USART5->RDR;
+      to_instruction(&instruction, rx);
 
-      // USART5->TDR = rx;
-      // // send_string("blah");
+      if (instruction.prefix == 0x3) {
+        send_string("got h\n");
+      }
 
-      if (rx == RESET_CONFIG) {
-        state = IDLE;
+      if (instruction.prefix == 0x2) {
+        send_string("got H\n");
+      }
+
+      // if (rx == RESET_CONFIG) {
+      //   state = IDLE;
         
-        WAIT_FOR_UART_TX;
-        // USART5->TDR = RESET_SUCCESS;
-        send_string("reseting"); // TEMP
-      }
+      //   WAIT_FOR_UART_TX;
+      //   // USART5->TDR = RESET_SUCCESS;
+      //   send_string("reseting"); // TEMP
+      // }
 
-      switch (state) {
-        case IDLE:
-          if (rx == START_CONFIG) { 
-            state = STARTING;
-            send_string("starting"); // TEMP
-          }
-          break;
-        case ERROR_STATE:
+      // switch (state) {
+      //   case IDLE:
+      //     if (rx == START_CONFIG) { 
+      //       state = STARTING;
+      //       send_string("starting"); // TEMP
+      //     }
+      //     break;
+      //   case ERROR_STATE:
 
-        case STARTING:
-          if (rx == START_CONFIG) { 
-            WAIT_FOR_UART_TX;
-            USART5->TDR = READY_TO_START;
-            send_string("UART_CONFIG"); // TEMP
-            state = UART_CONFIG;
-          }
-          break;
-        case UART_CONFIG:
-          if (rx == START_CB) { }
-          else if (rx == START_POT) { }
-          else if (rx == END_CONFIG) { }
-          break;
-        case CHOOSE_CB_CONNS: break;
-        case CHOOSE_POS_RES: break;
-        case POST_POT_CONFIG: break;
-        case POST_BOARD_CONFIG: break;
-        // case ANALOG_CONFIG: break;
-        default: break;
-      }
+      //   case STARTING:
+      //     if (rx == START_CONFIG) { 
+      //       WAIT_FOR_UART_TX;
+      //       USART5->TDR = READY_TO_START;
+      //       send_string("UART_CONFIG"); // TEMP
+      //       state = UART_CONFIG;
+      //     }
+      //     break;
+      //   case UART_CONFIG:
+      //     if (rx == START_CB) { }
+      //     else if (rx == START_POT) { }
+      //     else if (rx == END_CONFIG) { }
+      //     break;
+      //   case CHOOSE_CB_CONNS: break;
+      //   case CHOOSE_POS_RES: break;
+      //   case POST_POT_CONFIG: break;
+      //   case POST_BOARD_CONFIG: break;
+      //   // case ANALOG_CONFIG: break;
+      //   default: break;
+      // }
 
       // if (!in_packet) {
       //   send_string("not in packet");
@@ -233,46 +242,6 @@ void SysTick_Handler(void)
   HAL_IncTick();
 }
 
-
-/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    OLD UART CODE
-   -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
-  // var inits for loop
-  // uint8_t arrays[9][128];
-  // int current_array = -1;
-  // bool in_packet = false;
-
-  //    in while loop vvvvv
-  //      	if ((USART5->ISR & USART_ISR_RXNE)) {
-  //   			uint8_t rx = USART5->RDR;
-    
-  //   			if (!in_packet) {
-  //   				send_string("not in packet");
-  //   				if (rx >= 1 && rx <= 9) {
-  //   					current_array = rx - 1;
-  //   					for(int i=0; i<128; i++) arrays[current_array][i] = 0;
-  //   				}
-  //   				else if (rx == START_STOP_CHAR && current_array != -1) {
-  //   					in_packet = true;
-  //   				}
-  //   			}
-  //   			else {
-  //   				if (rx == START_STOP_CHAR) {
-  //   					in_packet = false;
-  //   					send_string("array updated");
-  //   					send_array(arrays[current_array], 128);
-  //   					current_array = -1;
-    
-  //   				}
-  //   				else if (rx <= 127 && current_array != -1) {
-  //   					arrays[current_array][rx] = 1;
-  //   				}
-  //   			}
-    
-  //   			USART5->TDR = rx;
-  //   			while(!(USART5->ISR & USART_ISR_TC));
-  //      	}
-
 void send_array(uint8_t *data, uint32_t size)
 {
     for (uint32_t i = 0; i < size; i++) {
@@ -291,47 +260,18 @@ void send_string(char *str)
     }
 }
 
-/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    OLD DIGITAL POT CODE
-   -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
-
-//    setup_blinker();
-//      // enabling all connections in tcon
-//   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 0);
-//   write_register(TCON_REG, 0x41FF, &hspi);
+// void setup_blinker(void) {
+//   __HAL_RCC_GPIOC_CLK_ENABLE();
+//   GPIO_InitTypeDef GPIO_InitStruct = {0};
+//   GPIO_InitStruct.Pin = GPIO_PIN_6;
+//   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+//   GPIO_InitStruct.Pull = GPIO_NOPULL;
+//   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+//   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 //   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 1);
-
-//   // HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 0); // manually framing w/ CS is needed rn, will be done by shift registers on real board
-//   // set_resistance(V_WIPER_0, MAX_RES, &hspi);
-//   // HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 1);
-
-
-//   while (1)
-//   {
-//     HAL_Delay(3000);
-//     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 0);
-//     set_resistance(V_WIPER_0, MAX_RES, &hspi);
-//     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 1);
-
-//     HAL_Delay(3000);
-//     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 0);
-//     set_resistance(V_WIPER_0, MIN_RES, &hspi);
-//     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 1);
-//     // HAL_Delay(2000);
-//     // decr_resistance(V_WIPER_0, &hspi);
-
-    
-//   }
 // }
 
-
-void setup_blinker(void) {
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  GPIO_InitStruct.Pin = GPIO_PIN_6;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 1);
+static inline void to_instruction(instruction_t *instruction, uint8_t value) {
+    instruction->prefix  = value >> 5 & 0x7;
+    instruction->message = value & 0x1F;
 }
