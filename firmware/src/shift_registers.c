d59e2fd6ef59114035d3f77a5916d24b3a40e3e7
MicahSamuel77 <m.samuel0922@gmail.com>
2026-04-08 12:04:02 -0400

    feat: redo sr drivers

--- firmware/src/shift_registers.c
+++ firmware/src/shift_registers.c
@@ -1,35 +1,45 @@
 #include "shift_registers.h"
 #include "stm32f0xx_hal.h"
 
-
-void reset_cd22m_sr(void) {
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 0);
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 1);
+sr_reset(sr_t sr) {
+  if (sr == BRUZ_SR) {
+    // bruz is ~cs
+    sr_set(sr, 1);
+    for (int i = 0; i < 8; i++) sr_clock(sr);
+  } else {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 0);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 1);
+  }
 }
 
-void reset_bruz_sr(void) {
-
-  // bruz is ~cs
-  set_bruz_sr_data(1);
-
-  for (int i = 0; i < 8; i++) clock_bruz_sr();
+sr_set(sr_t sr, int data) {
+  if (sr == BRUZ_SR)  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, data);
+  else                HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, data);
 }
 
-void set_bruz_sr_data(int data) {
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, data);
+sr_clock(sr_t sr) {
+  if (sr == BRUZ_SR) {
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 1);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
+  } else {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 1);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 0);
+  }
 }
 
-void set_cd22m_sr_data(int data) {
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, data);
+sr_start(sr_t sr) {
+  if (sr == BRUZ_SR) {
+    sr_set(sr, 0);
+    sr_clock(sr);
+    sr_set(sr, 1);
+  } else {
+    sr_set(sr, 1);
+    sr_clock(sr);
+    sr_set(sr, 0);
+  }
+
 }
 
-void clock_bruz_sr(void) {
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 1);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
+sr_shift_en(sr_t sr) {
+  sr_clock(sr);
 }
-
-void clock_cd22m_sr(void) {
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 1);
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 0);
-}
-
